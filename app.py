e999cffb4bc7132d2852fe6d2f66351e88827974
Justyna <justyna.chmielinska@coderscrew.pl>
2021-12-19 23:09:42 +0100

    Added lots of chonkers

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-import os
+
+from cs50 import SQL
 import sqlite3
 
-from flask import Flask, flash, redirect, render_template, request, session
+from flask import Flask, redirect, render_template, request, session
 from flask_session import Session
 from tempfile import mkdtemp
+from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
 
@@ -24,9 +26,96 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Database
-con = sqlite3.connect('chonkers.db')
+con = SQL('sqlite:///chonkers.db')
+
+
+# Error message
+def apology(message, code=400):
+    return render_template('apology.html')
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+
+        # Query database for username
+        rows = con.execute(
+            "SELECT * FROM Users WHERE Username = ?", request.form.get("username"))
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["Password"], request.form.get("password")):
+            return apology("invalid username and/or password", 403)
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["UserId"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user"""
+    if request.method == "POST":
+
+        if not request.form.get("username"):
+            return apology("must provide username", 400)
+
+        elif len(con.execute("SELECT * FROM Users WHERE Username = ?", request.form.get("username"))) != 0:
+            return apology("Username already exists, please choose another", 400)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 400)
+
+        elif not request.form.get("confirmation") or (request.form.get("confirmation") != request.form.get("password")):
+            return apology("both password fields must match", 400)
+
+        pw_hash = generate_password_hash(request.form.get("password"))
+
+        rows = con.execute("INSERT INTO Users (Username, Password) VALUES (?, ?)",
+                           request.form.get("username"), pw_hash)
+
+        return redirect("/login")
+    else:
+        return render_template("register.html")
+
+
+@app.route('/vote', methods=["GET", "POST"])
+def vote():
+    if request.method == "POST":
+        return
+    return redirect('/')
